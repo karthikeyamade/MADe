54286ff7ea88ab0356d63450c457b1289d2c3382
karthikeyamade <55698699+karthikeyamade@users.noreply.github.com>
2020-07-23 13:46:32 +0000

    Update phn.py

--- phn.py
+++ phn.py
@@ -44,8 +44,4 @@ if __name__ == "__main__":
 	#set2 = ['A', 'B', 'C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
 	set2 = ['1','2','3','4','5','6','7','8','9','0']
 	k = 9
-	printAllKLength(set2, k) 
-
-# This code is contributed 
-# by ChitraNayal 
-
+	printAllKLength(set2, k)
